c692fedf55d8a381bb7eace3f4b2bfeb71eb6f61
Joseph Cracchiolo <jcracchiolo@tucows.com>
2024-07-22 15:52:22 -0700

    Need string conversion for all numbers (Zabbix doesn't do real JSONRPC).

--- host_interface.go
+++ host_interface.go
@@ -24,10 +24,10 @@ const (
 type HostInterface struct {
 	DNS   string        `json:"dns"`
 	IP    string        `json:"ip"`
-	Main  int           `json:"main"`
+	Main  int           `json:"main,string"`
 	Port  string        `json:"port"`
-	Type  InterfaceType `json:"type"`
-	UseIP int           `json:"useip"`
+	Type  InterfaceType `json:"type,string"`
+	UseIP int           `json:"useip,string"`
 }
 
 // HostInterfaces is an array of HostInterface
